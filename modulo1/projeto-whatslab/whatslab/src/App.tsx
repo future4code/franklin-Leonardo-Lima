45f1290820d67c92e1b8b03d8dfb9c3726da5b3c
Leonardo Santino <79323700+leonardosantino@users.noreply.github.com>
2022-05-27 12:05:16 -0300

    feat: add create react app whatslab

---
+++ modulo1/projeto-whatslab/whatslab/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { WhatsLab } from './pages/WhatsLab';
+
+function App() {
+  return (
+    <>
+      <WhatsLab></WhatsLab>
+    </>
+  );
+}
+
+export default App;
