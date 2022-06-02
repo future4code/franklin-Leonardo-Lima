c2657bb019e49fa2ea2772d98e6b41cc73a3c594
Leonardo Santino <79323700+leonardosantino@users.noreply.github.com>
2022-06-02 12:08:54 -0300

    feat: add project Insta4 (#10)

---
+++ modulo1/estados-insta4/insta4/src/components/share/Share.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import share from '../../img/share.png';
+
+export function Share() {
+
+    const [text, setText] = useState('');
+
+    function handleClickSelect(e){
+        console.log(`Post Compartilhado no ${e.target.innerHTML}: ${text}`)
+    }
+
+    function handleClick(e) {
+        setText(e.target.value)
+    }
+    
+    return (
+        <>
+            <div className="dropdown">
+                <img alt='' src={share} id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                </img>
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1" >
+                    <li onClick={handleClickSelect}><a className="dropdown-item" href="#1">Instagram</a></li>
+                    <li onClick={handleClickSelect}><a className="dropdown-item" href="#2">Facebook</a></li>
+                    <li onClick={handleClickSelect}><a className="dropdown-item" href="#3">Twiter</a></li>
+                    <input onChange={handleClick} type="text" name="" id="" placeholder='Digite sua mensagem'/>
+                </ul>
+            </div>
+        </>
+    )
+}
