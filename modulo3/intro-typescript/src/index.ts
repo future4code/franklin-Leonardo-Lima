11c4fa4fd19ad6a46c9d28b667c648e76611d21e
Leonardo Santino <79323700+leonardosantino@users.noreply.github.com>
2022-08-08 15:35:04 -0300

    feat: intro-typescript (#20)
    Co-authored-by: Leonardo Santino <leonardosantio@live.com>

---
+++ modulo3/intro-typescript/src/index.ts
@@ -0,0 +1,60 @@
+//escreva o seu código aqui
+
+const promptSync = require('prompt-sync')();
+
+function checaTriangulo(a: number, b: number, c: number): string {
+  if (a !== b && b !== c) {
+    return "Escaleno";
+  } else if (a === b && b === c) {
+    return "Equilátero";
+  } else {
+    return "Isósceles";
+  }
+}
+
+function imprimeTresCoresFavoritas(): void {
+  const cor1: string = promptSync("Insira sua primeira cor favorita");
+  const cor2: string = promptSync("Insira sua segunda cor favorita");
+  const cor3: string = promptSync("Insira sua terceira cor favorita");
+  console.log([cor1, cor2, cor3]);
+}
+
+function checaAnoBissexto(ano: number): boolean {
+  const cond1: boolean = ano % 400 === 0;
+  const cond2: boolean = ano % 4 === 0 && ano % 100 !== 0;
+  return cond1 || cond2;
+}
+
+function comparaDoisNumeros(num1: number, num2: number): number {
+  let maiorNumero: number;
+  let menorNumero: number;
+
+  if (num1 > num2) {
+    maiorNumero = num1;
+    menorNumero = num2;
+  } else {
+    maiorNumero = num2;
+    menorNumero = num1;
+  }
+
+  const diferenca: number = maiorNumero - menorNumero;
+
+  return diferenca;
+}
+
+function checaRenovacaoRG(): boolean {
+  const anoAtual: number = Number(promptSync("Digite o ano atual"));
+  const anoNascimento: number = Number(promptSync("Digite o ano de nascimento"));
+  const anoEmissao: number = Number(
+    promptSync("Digite o ano de emissão do documento")
+  );
+
+  const idade: number = anoAtual - anoNascimento;
+  const tempoCarteira: number = anoAtual - anoEmissao;
+
+  const cond1: boolean = idade <= 20 && tempoCarteira >= 5;
+  const cond2: boolean = idade > 20 && idade <= 50 && tempoCarteira >= 10;
+  const cond3: boolean = idade > 50 && tempoCarteira >= 15;
+
+  return cond1 || cond2 || cond3;
+}
