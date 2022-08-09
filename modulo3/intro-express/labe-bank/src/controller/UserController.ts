07fd0c5c41a733b8f41ef12fcdcaeaeef7dd3464
Leonardo Santino <leonardosantio@live.com>
2022-08-09 20:00:10 -0300

    fix: remove another project

--- modulo3/intro-express/labe-bank/src/controller/UserController.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-import { usersData } from '../../data';
-
-export default class UserController {
-  get(request: Request, response: Response) {
-    return response.status(200).send(usersData);
-  }
-}
