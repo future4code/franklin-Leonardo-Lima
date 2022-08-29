07c9f6b5aab6c79d9aef1394757f77782fedf933
Leonardo Santino <leonardosantio@live.com>
2022-08-29 11:45:23 -0300

    fix: code format

--- modulo3/classes-encapsulamento/Bank.ts
+++ modulo3/classes-encapsulamento/Bank.ts
@@ -1,14 +1,13 @@
 class Bank {
-    private _accounts: UserAccount[];
-    public get accounts(): UserAccount[] {
-        return this._accounts;
-    }
-    public set accounts(value: UserAccount[]) {
-        this._accounts = value;
-    }
-  
-    constructor(accounts: UserAccount[]) {
-      this.accounts = accounts;
-    }
-  
+  private _accounts: UserAccount[];
+  public get accounts(): UserAccount[] {
+    return this._accounts;
   }
+  public set accounts(value: UserAccount[]) {
+    this._accounts = value;
+  }
+
+  constructor(accounts: UserAccount[]) {
+    this.accounts = accounts;
+  }
+}
