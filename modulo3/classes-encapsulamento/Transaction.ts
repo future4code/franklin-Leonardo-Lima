b05db897d410707077f04e1e0cd50e1efde50d21
Leonardo Santino <79323700+leonardosantino@users.noreply.github.com>
2022-09-12 16:08:29 -0300

    feat: classes encapsulamento (#28)
    * feat: classes encapsulamento

---
+++ modulo3/classes-encapsulamento/Transaction.ts
@@ -0,0 +1,29 @@
+class Transaction {
+  private _date: string;
+  public get date(): string {
+    return this._date;
+  }
+  public set date(value: string) {
+    this._date = value;
+  }
+  private _value: number;
+  public get value(): number {
+    return this._value;
+  }
+  public set value(value: number) {
+    this._value = value;
+  }
+  private _description: string;
+  public get description(): string {
+    return this._description;
+  }
+  public set description(value: string) {
+    this._description = value;
+  }
+
+  constructor(date: string, value: number, description: string) {
+    this.date = date;
+    this.value = value;
+    this.description = description;
+  }
+}
